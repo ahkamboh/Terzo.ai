bc4bdf211450438d829d2d678383fc269c635fa4
Ali Hamza Kamboh <123060177+ahkamboh@users.noreply.github.com>
2023-06-23 01:51:44 +0500

    Initial

--- assets/script.js
+++ assets/script.js
@@ -59,25 +59,3 @@ resgen = () => {
   textc.classList.remove("text-[#bb99ff]");
   textC.classList.remove("text-[#bb99ff]");
 };
-  // chat button api
- var chatbox = document.getElementById('fb-customer-chat');
-      chatbox.setAttribute("page_id", "100425223107136");
-      chatbox.setAttribute("attribution", "biz_inbox");
-    </script>
-
-    <!-- Your SDK code -->
-    <script>
-      window.fbAsyncInit = function() {
-        FB.init({
-          xfbml            : true,
-          version          : 'v17.0'
-        });
-      };
-
-      (function(d, s, id) {
-        var js, fjs = d.getElementsByTagName(s)[0];
-        if (d.getElementById(id)) return;
-        js = d.createElement(s); js.id = id;
-        js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
-        fjs.parentNode.insertBefore(js, fjs);
-      }(document, 'script', 'facebook-jssdk'));
